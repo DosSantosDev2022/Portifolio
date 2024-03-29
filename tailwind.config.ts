87a1cea986b9be3683edeb94814059196471b959
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-03-29 20:19:13 -0300

    feat : criando váriaveis de cores no theme tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      fontFamily: {},
+      colors: {
+        primary: '#09090b',
+        primary_hover: '#18181b',
+        secundary: '#27272a',
+        secundary_hover: '#3f3f46',
+        highlights: '#5b21b6',
+        highlights_hover: '#6d28d9',
+        light: '#fafafa',
+        lightSilver: '#d4d4d8',
+      },
     },
   },
   plugins: [],

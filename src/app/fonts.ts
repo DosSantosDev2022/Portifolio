cf4e1e18d705279ac56322cb9ffeb81e24a39a65
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-03-29 20:18:06 -0300

    feat : criando arquivo para fontes do projeto

---
+++ src/app/fonts.ts
@@ -0,0 +1,12 @@
+/* eslint-disable camelcase */
+import { Manrope, Bebas_Neue } from 'next/font/google'
+
+export const manrope = Manrope({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+})
+
+export const bebas = Bebas_Neue({
+  weight: '400',
+  subsets: ['latin'],
+})

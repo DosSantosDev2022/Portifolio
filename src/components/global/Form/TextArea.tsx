028ca611a6d75ba424fccad354891f00d87a5961
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-03-31 21:47:51 -0300

    styles : corrigindo cor da borda do componente textarea

--- src/components/global/Form/TextArea.tsx
+++ src/components/global/Form/TextArea.tsx
@@ -6,7 +6,7 @@ export function TextArea({ ...props }: TextAreaProps) {
   return (
     <textarea
       {...props}
-      className="bg-border rounded border border-zinc-900 px-4 py-3 text-lightSilver placeholder:text-zinc-400"
+      className="bg-border rounded border border-zinc-700 px-4 py-3 text-lightSilver placeholder:text-zinc-400"
     />
   )
 }

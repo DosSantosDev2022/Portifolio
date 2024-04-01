781d4ed01e85133a0760aa3f3fad490d1faaca25
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-03-31 21:42:28 -0300

    feat : criando componentes input, label e textarea

---
+++ src/components/global/Form/Input.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={twMerge(
+          'bg-border flex h-10 w-full rounded-md border border-zinc-700 px-3 text-lightSilver placeholder:text-zinc-400',
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  },
+)
+Input.displayName = 'Input'
+
+export { Input }
---
+++ src/components/global/Form/TextArea.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export type TextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>
+
+export function TextArea({ ...props }: TextAreaProps) {
+  return (
+    <textarea
+      {...props}
+      className="bg-border rounded border border-zinc-900 px-4 py-3 text-lightSilver placeholder:text-zinc-400"
+    />
+  )
+}

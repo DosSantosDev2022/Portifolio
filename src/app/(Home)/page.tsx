4f9fef688048adc06b150a3736a24c58778666c4
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-04-02 01:40:27 -0300

    styles : corrigindo estilos da pagina home

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -2,22 +2,20 @@ import { GET_DATA_HOME } from '@/app/Api/queries/Get_data_home'
 import { CMSIcon } from '@/components/global/CmsIcon'
 import { bebas } from '@/app/fonts'
 import { RiArrowRightUpLine } from 'react-icons/ri'
-import Image from 'next/image'
-import { LuArrowUpRight, LuLayout } from 'react-icons/lu'
-import { FaGithub } from 'react-icons/fa'
 
 import Link from 'next/link'
 import { Button } from '@/components/global/button'
-import { TooltipComponent } from '@/components/global/Tooltip'
 import { RichText } from '@/components/global/RichText'
+import { ProjectCard } from '@/components/global/ProjectCard'
+import Image from 'next/image'
 
 export default async function Home() {
   const { home } = await GET_DATA_HOME()
-  const techs = home.featuredProjects.projects.find((i) => i.technologie)
+  /* const techs = home.featuredProjects.projects.find((i) => i.technologie) */
 
   return (
     <section className="">
-      <div className=" mt-10 flex flex-col items-center justify-between gap-16 lg:h-screen lg:flex-row lg:gap-0 ">
+      <div className=" my-20 flex flex-col items-start justify-between gap-16  lg:flex-row lg:gap-0 ">
         <div className="flex flex-col items-start text-light lg:w-[544px]">
           <h1
             className={`text-6xl font-normal leading-[57px] lg:text-[6rem] lg:leading-[90.9px] ${bebas.className} `}
@@ -54,14 +52,16 @@ export default async function Home() {
         <div className="h-[470px] w-full flex-shrink-0 rounded-2xl bg-lightSilver lg:w-[450px]"></div>
       </div>
 
-      <div className="mt-10 flex w-full flex-col items-center  justify-between gap-3 border-t-2 border-zinc-800 lg:h-screen lg:flex-row">
+      <div className="my-20 flex w-full flex-col items-center  justify-between gap-3 lg:flex-row">
         <div className="flex w-full flex-col items-start gap-2">
           <h3
             className={`${bebas.className}  w-full text-6xl font-normal leading-[5.68125rem] text-light lg:text-[6.3125rem]`}
           >
             {home.sectionAboutMe.title}
           </h3>
-          <div className="mt-3 h-[400px] w-full flex-shrink-0 rounded-2xl bg-lightSilver lg:w-[350px]"></div>
+          <div className="mt-3 h-[400px] w-full flex-shrink-0 rounded-2xl bg-lightSilver lg:w-[350px]">
+            <Image src={'/profile.png'} alt="" width={343} height={400} />
+          </div>
         </div>
 
         <div className="mt-6 flex w-full flex-col items-start gap-2">
@@ -93,7 +93,7 @@ export default async function Home() {
         </div>
       </div>
 
-      <div className="mt-10 flex flex-col gap-2 border-t-2 border-zinc-800">
+      <div className="my-20 flex flex-col gap-2 ">
         <div className="mt-6 flex flex-col items-start gap-2 lg:w-[37rem]">
           <h2
             className={` ${bebas.className} text-6xl font-normal  text-light lg:text-[4.75rem] lg:leading-[4.75rem] `}
@@ -104,84 +104,19 @@ export default async function Home() {
             {home.featuredProjects.smallText}{' '}
           </p>
         </div>
-        <div className="mt-10 flex flex-col gap-[7.5rem] lg:mt-20 ">
+        <div className="mt-10 flex flex-col gap-10  lg:mt-20 lg:flex-row ">
           {home.featuredProjects.projects.map((project) => (
-            <div
+            <ProjectCard
               key={project.title}
-              className="flex w-full flex-col items-center gap-8 lg:flex-row "
-            >
-              <Image
-                alt={project.title}
-                src={project.coverImage.url}
-                width={450}
-                height={450}
-                quality={100}
-                className=" rounded-md bg-zinc-400"
-              />
-              <div className="w-full items-center space-y-4 lg:items-start">
-                <h3 className="text-[2rem] font-medium leading-[2.8rem] text-light ">
-                  {project.title}
-                </h3>
-                <p className="text-lg font-normal leading-[1.6875rem] text-lightSilver ">
-                  {project.description}
-                </p>
-
-                <h4 className="text-base font-normal uppercase leading-[1.6rem] text-light ">
-                  Tecnologias utilizadas
-                </h4>
-
-                <div className="flex flex-wrap items-center gap-6 border-b border-t border-zinc-800 px-1 pb-2 pt-2">
-                  {techs?.technologie.map((tech) => (
-                    <TooltipComponent key={tech.id} content={tech.name}>
-                      <Image
-                        key={tech.id}
-                        alt={tech.name}
-                        src={tech.icon.url}
-                        width={38}
-                        height={38}
-                        quality={100}
-                        className="rounded-md  bg-zinc-500  p-1"
-                      />
-                    </TooltipComponent>
-                  ))}
-                </div>
-
-                <div className="mt-8 flex w-full gap-3">
-                  <Button asChild variant="outline">
-                    <Link
-                      className="flex items-center gap-1 text-highlights duration-300 hover:bg-highlights_hover"
-                      href={''}
-                    >
-                      <span>Ver mais</span>
-                      <LuArrowUpRight />
-                    </Link>
-                  </Button>
-
-                  <Button asChild variant="outline">
-                    <Link
-                      className="flex items-center gap-1 text-highlights duration-300 hover:bg-highlights_hover"
-                      href={''}
-                    >
-                      <span>Ver demo</span>
-                      <LuLayout />
-                    </Link>
-                  </Button>
-
-                  <Button asChild variant="outline">
-                    <Link
-                      className="flex items-center gap-1 text-highlights duration-300 hover:bg-highlights_hover"
-                      href={''}
-                    >
-                      <span>Código fonte</span>
-                      <FaGithub />
-                    </Link>
-                  </Button>
-                </div>
-              </div>
-            </div>
+              title={project.title}
+              url={''}
+              description={project.description}
+              coverImage={project.coverImage.url}
+              id=""
+            />
           ))}
         </div>
-        <div className="mt-10  flex w-full items-center justify-center border-b-2 border-zinc-800 p-2 ">
+        <div className="mt-10  flex w-full items-center justify-end p-2 ">
           <Button asChild className="mb-10" variant="outline">
             <Link href={'/Projects'}>Ver todos...</Link>
           </Button>

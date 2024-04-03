2a6775c0807f37112f050f9cd11c2ac684061768
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2024-04-02 21:01:03 -0300

    feat : refatorando componente textarea

--- src/components/global/Form/TextArea.tsx
+++ src/components/global/Form/TextArea.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 
 export type TextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  (props, ref) => {
+    return (
+      <textarea
+        {...props}
+        ref={ref}
+        className="rounded border border-zinc-700 bg-border px-4 py-3 text-lightSilver placeholder:text-zinc-400"
+      />
+    )
+  },
+)
 
-export function TextArea({ ...props }: TextAreaProps) {
-  return (
-    <textarea
-      {...props}
-      className="bg-border rounded border border-zinc-700 px-4 py-3 text-lightSilver placeholder:text-zinc-400"
-    />
-  )
-}
+TextArea.displayName = 'textArea'
+
+export default TextArea

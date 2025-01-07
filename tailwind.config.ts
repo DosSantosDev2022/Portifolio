4b17e41be81d8713f97038a9fb251603126ce17d
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2025-01-07 14:25:30 -0300

    refactor: adicionando template colors Model ChromaUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,15 +9,41 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: '#18181b',
-        primary_hover: '#18181b',
-        secundary: '#27272a',
-        secundary_hover: '#3f3f46',
-        highlights: '#5b21b6',
-        highlights_hover: '#6d28d9',
-        light: '#fafafa',
-        lightSilver: '#d4d4d8',
-        border_color: '#3f3f46',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+
+        primary: 'var(--primary)',
+        'primary-hover': 'var(--primary-hover)',
+        'primary-foreground': 'var(--primary-foreground)',
+        secondary: 'var(--secondary)',
+        'secondary-hover': 'var(--secondary-hover)',
+        'secondary-foreground': 'var(--secondary-foreground)',
+        muted: 'var(--muted)',
+        'muted-hover': 'var(--muted-hover)',
+        'muted-foreground': 'var(--muted-foreground)',
+        accent: 'var(--accent)',
+        'accent-hover': 'var(--accent-hover)',
+        'accent-foreground': 'var(--accent-foreground)',
+
+        danger: 'var(--danger)',
+        'danger-hover': 'var(--danger-hover)',
+        'danger-foreground': 'var(--danger-foreground)',
+        warning: 'var(--warning)',
+        'warning-hover': 'var(--warning-hover)',
+        'warning-foreground': 'var(--warning-foreground)',
+        success: 'var(--success)',
+        'success-hover': 'var(--success-hover)',
+        'success-foreground': 'var(--success-foreground)',
+
+        border: 'var(--border)',
+        input: 'var(--input)',
+        ring: 'var(--ring)',
+
+        'chart-1': 'var(--chart-1)',
+        'chart-2': 'var(--chart-2)',
+        'chart-3': 'var(--chart-3)',
+        'chart-4': 'var(--chart-4)',
+        'chart-5': 'var(--chart-5)',
       },
       backgroundImage: {
         'gradient-color':

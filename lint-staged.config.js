6d93081144020d3713b9d77758fcd2ab8183318e
Dos Santos Conceicao <Juliano.Santos@la.imtn.com>
2025-03-18 14:20:07 -0300

    fix: corrigindo imports

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,6 @@
 export default {
-  '*.js': ['eslint --fix', 'prettier --write'],
-  '*.jsx': ['eslint --fix', 'prettier --write'],
-  '*.ts': ['eslint --fix', 'prettier --write'],
-  '*.tsx': ['eslint --fix', 'prettier --write'],
-  '*.json': ['prettier --write'],
-  '*.css': ['prettier --write'],
-  '*.md': ['prettier --write']
+	'*.{js,jsx,ts,tsx,json,css,md}': [
+		'biome format --write',
+		'biome lint --apply',
+	],
 }
--- src/@types/homePage.ts
+++ src/@types/homePage.ts
@@ -43,7 +43,7 @@ export interface homePageTypes {
 			id: string
 			title: string
 			icon: {
-				 url: string
+				url: string
 			}
 			description: string
 		}[]

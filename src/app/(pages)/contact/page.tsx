e015d0219984c3c8d2563025bec9b51db7cad71e
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2025-06-07 13:16:36 -0300

    feat: adicionando metadata nas pages

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,116 +1,84 @@
-'use client'
 import { ScrollAnimation } from '@/components/global'
-import { useForm, type SubmitHandler } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Button, Input, Label, TextArea } from '@/components/ui'
-import { useState } from 'react'
-import { ImSpinner9 } from 'react-icons/im'
-import { useNotification } from '@/context/notification/notificationContext'
-import { type FormData, FormSchema } from '@/@types/forms'
+import { FormContact } from '@/components/global/form'
 
-export default function Contact() {
-	const {
-		register,
-		reset,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<FormData>({
-		resolver: zodResolver(FormSchema),
-	})
-	const { showNotification } = useNotification()
-	const [isLoading, setIsLoading] = useState(false)
+export function metadata() {
+	return {
+		title: 'Portifílio - Contato',
+		description:
+			'Olá! Sou um apaixonado desenvolvedor Full Stack com experiência em diversas tecnologias modernas da web. Minha jornada no mundo da programação começou com uma paixão pelo front-end, e ao longo dos anos, expandi meu conhecimento para abranger também o back-end, tornando-me um desenvolvedor versátil e capaz de criar soluções completas e eficientes.',
+		openGraph: {
+			title: 'Portifílio - DosSantosDev',
+			description:
+				'Olá! Sou um apaixonado desenvolvedor Full Stack com experiência em diversas tecnologias modernas da web. Minha jornada no mundo da programação começou com uma paixão pelo front-end, e ao longo dos anos, expandi meu conhecimento para abranger também o back-end, tornando-me um desenvolvedor versátil e capaz de criar soluções completas e eficientes.',
+			url:
+				process.env.NEXT_PUBLIC_SITE_URL || 'https://dossantosdev.com.br/',
+			siteName: 'Meu Portfólio / DosSantosdev',
+			images: [
+				{
+					url: '/images/author.png',
+					width: 1200,
+					height: 630,
+					alt: 'Juliano Santos, desenvolvedor FullStack',
+				},
+			],
+			locale: 'pt_BR',
+			type: 'website',
+		},
 
-	const onSubmit: SubmitHandler<FormData> = async (data) => {
-		setIsLoading(true)
-		const response = await fetch('api/send', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
+		twitter: {
+			card: 'summary_large_image',
+			title: 'Portifílio - DosSantosDev',
+			description:
+				'Olá! Sou um apaixonado desenvolvedor Full Stack com experiência em diversas tecnologias modernas da web. Minha jornada no mundo da programação começou com uma paixão pelo front-end, e ao longo dos anos, expandi meu conhecimento para abranger também o back-end, tornando-me um desenvolvedor versátil e capaz de criar soluções completas e eficientes.',
+			images: ['/images/author.png'],
+		},
+
+		icons: {
+			icon: '/favicon.ico',
+			shortcut: '/favicon-16x16.png',
+			apple: '/apple-touch-icon.png',
+		},
+
+		robots: {
+			index: true,
+			follow: true,
+			nocache: true,
+			googleBot: {
+				index: true,
+				follow: true,
+				noimageindex: true,
+				'max-video-preview': -1,
+				'max-image-preview': 'large',
+				'max-snippet': -1,
 			},
-			body: JSON.stringify(data),
-		})
+		},
 
-		setIsLoading(false)
-		if (response.ok) {
-			showNotification('Email enviado com sucesso!', 'success')
-		} else {
-			showNotification('Email enviado com sucesso!', 'error')
-		}
-		reset()
+		// Opcional: Autor
+		// authors: [{ name: 'Seu Nome', url: 'https://seusite.com/sobre' }],
+
+		keywords: [
+			'desenvolvimento web',
+			'desenvolvimento mobile',
+			'tailwindcss',
+			'react',
+			'next.js',
+			'portfólio',
+			'programação',
+			'frontend',
+			'fullstack',
+			'backend',
+		],
 	}
+}
 
+export default async function Contact() {
 	return (
 		<ScrollAnimation className='flex flex-col gap-10 p-4 lg:px-10 lg:py-8'>
 			<div className=' w-full lg:max-w-3xl  space-y-10'>
 				<h2 className='text-start text-2xl font-bold  sm:text-4xl'>
 					Entre em contato para parcerias.
 				</h2>
-				<form onSubmit={handleSubmit(onSubmit)} className='space-y-6'>
-					<div className='flex flex-col gap-1'>
-						<Label htmlFor='nome'>Nome</Label>
-						<Input
-							{...register('nome')}
-							placeholder='Digite seu nome completo'
-							type='text'
-						/>
-
-						{errors && (
-							<span className='text-md text-danger font-normal'>
-								{errors.nome?.message}
-							</span>
-						)}
-					</div>
-					<div className='flex flex-col gap-1'>
-						<Label htmlFor='email'>Email</Label>
-						<Input
-							{...register('email')}
-							placeholder='Digite um e-mail válido'
-							type='email'
-						/>
-						{errors && (
-							<span className='text-md text-danger font-normal'>
-								{errors.email?.message}
-							</span>
-						)}
-					</div>
-					<div className='flex flex-col gap-1'>
-						<Label htmlFor='telefone'>Telefone</Label>
-						<Input
-							{...register('telefone')}
-							placeholder='(xx) xxxx-xxxxx'
-							type='tel'
-						/>
-						{errors && (
-							<span className='text-md text-danger font-normal'>
-								{errors.telefone?.message}
-							</span>
-						)}
-					</div>
-					<div className='flex flex-col gap-1'>
-						<Label htmlFor='mensagem'>Mensagem</Label>
-						<TextArea
-							{...register('mensagem')}
-							placeholder='Deixe sua mensagem...'
-						/>
-						{errors && (
-							<span className='text-md text-danger font-normal'>
-								{errors.mensagem?.message}
-							</span>
-						)}
-					</div>
-					<div className='flex w-full items-center justify-end'>
-						<Button variants='accent' disabled={isLoading} sizes='full'>
-							{isLoading ? (
-								<span className='flex items-center justify-center gap-1'>
-									Enviando...
-									<ImSpinner9 className='text-light animate-spin' />
-								</span>
-							) : (
-								'Enviar'
-							)}
-						</Button>
-					</div>
-				</form>
+				<FormContact />
 			</div>
 		</ScrollAnimation>
 	)

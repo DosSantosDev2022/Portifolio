58b3f8330dc9cd4fb2844fe248c5c5494e0074a4
DosSantosDev2022 <dossantosdevoficial@gmail.com>
2025-09-19 14:54:24 -0300

    fix: corrigindo sidebar props

--- src/components/global/sidebar/desktop-sidebar.tsx
+++ src/components/global/sidebar/desktop-sidebar.tsx
@@ -1,24 +1,33 @@
-"use client"
-import { UserProfile } from './user-profile';
-import { SidebarNav } from './sidebar-nav';
-import { navLinks, socialLinks } from '@/config/sidebar-links';
+'use client'
+import { UserProfile } from './user-profile'
+import { SidebarNav } from './sidebar-nav'
+import { navLinks, socialLinks } from '@/config/sidebar-links'
 
 const DesktopSidebar = () => {
-  return (
-    <aside className='hidden md:flex md:w-64 lg:w-72 flex-col fixed inset-y-0 z-10 border-r p-4'>
-      <div className='flex h-full w-full flex-col'>
-        <div className='mb-4'>
-          <UserProfile 
-          />
-        </div>
-        
-        <nav className='flex-1 space-y-8'>
-          <SidebarNav isSocial={false} title={''} links={navLinks} />
-          <SidebarNav links={socialLinks} title="Social" isSocial />
-        </nav>
-      </div>
-    </aside>
-  );
+	return (
+		<aside className='hidden md:flex md:w-64 lg:w-72 flex-col fixed inset-y-0 z-10 border-r p-4'>
+			<div className='flex h-full w-full flex-col'>
+				<div className='mb-4'>
+					<UserProfile />
+				</div>
+
+				<nav className='flex-1 space-y-8'>
+					<SidebarNav
+						isMobile={false}
+						isSocial={false}
+						title={''}
+						links={navLinks}
+					/>
+					<SidebarNav
+						isMobile={false}
+						links={socialLinks}
+						title='Social'
+						isSocial
+					/>
+				</nav>
+			</div>
+		</aside>
+	)
 }
 
 export { DesktopSidebar }
